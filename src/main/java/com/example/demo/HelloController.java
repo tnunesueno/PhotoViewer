0fb3dbf3924a9a89ecd62f8f79ba4a33d8c296ea
tnunesueno <tnunesueno25@scienceleadership.org>
2024-11-22 09:21:50 -0500

    hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,14 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
-import javafx.scene.*;
 
 
-import javax.imageio.stream.FileImageInputStream;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
@@ -25,6 +22,8 @@ public class HelloController {
     public MenuItem button4;
     public MenuItem button5;
     public Slider slider;
+    // ADD CHOOOSE BUTTON
+
 
     int imageNum =1;
     @FXML
@@ -50,7 +49,6 @@ public class HelloController {
 
         pagination.setPageFactory((pageIndex) -> {
             imageNum = pageIndex;
-
             //splitmenu
             button1.setOnAction((event)-> {
                 pagination.setCurrentPageIndex(0);
@@ -67,12 +65,31 @@ public class HelloController {
             button5.setOnAction((event)-> {
                 pagination.setCurrentPageIndex(4);
             });
+            //slider
+          slider.setOnDragDetected(mouseEvent -> {
+                Double sliderValue = slider.getValue();
+                    if (sliderValue.intValue() <= 20) {
+                        pagination.setCurrentPageIndex(0);
+                    }
+                    if (sliderValue.intValue() >= 20 && sliderValue.intValue() <= 40) {
+                        pagination.setCurrentPageIndex(1);
+                    }
+                    if (sliderValue.intValue() >= 40 && sliderValue.intValue() <= 60) {
+                        pagination.setCurrentPageIndex(2);
+                    }
+                    if (sliderValue.intValue() >= 60 && sliderValue.intValue() <= 80) {
+                        pagination.setCurrentPageIndex(3);
+                    }
+                    if (sliderValue.intValue() >= 80 && sliderValue.intValue() <= 100) {
+                        pagination.setCurrentPageIndex(4);
+                }
+            });
+
+
             imageView.setImage(imageArray.get(imageNum));
          //   System.out.println("Hi Twyla " + pageIndex);
             return imageView;
         });
-
-
 
 
     }
