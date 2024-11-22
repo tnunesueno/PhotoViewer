e78d38cb139155e7a5eae74a0e95024d5c011831
tnunesueno <tnunesueno25@scienceleadership.org>
2024-11-22 08:38:26 -0500

    hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,23 +2,80 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.SplitMenuButton;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.*;
 
+
+import javax.imageio.stream.FileImageInputStream;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 public class HelloController {
+    public ImageView imageView;
+    public Pagination pagination;
+    public SplitMenuButton splitMenuButton;
+    public MenuItem button1;
+    public MenuItem button2;
+    public MenuItem button3;
+    public MenuItem button4;
+    public MenuItem button5;
+    public Slider slider;
+
+    int imageNum =1;
     @FXML
-    private Label welcomeText;
 
     public void initialize() throws Exception{
-        FileImputStream input1 = new FileImputStream("src/astro snoopy 2.png");
+        FileInputStream input1 = new FileInputStream("src/snoopy.png");
+        FileInputStream input2 = new FileInputStream("src/astro snoopy 2.png");
+        FileInputStream input3 = new FileInputStream("src/astro snoopy.jpeg");
+        FileInputStream input4 = new FileInputStream("src/Woodstock.png");
+        FileInputStream input5 = new FileInputStream("src/eli.png");
         Image image1 = new Image(input1);
-        ImageView imageView1 = new ImageView(image1);
+        Image image2 = new Image(input2);
+        Image image3 = new Image(input3);
+        Image image4 = new Image(input4);
+        Image image5 = new Image(input5);
+
+        ArrayList<Image> imageArray = new ArrayList<Image>();
+        imageArray.add(image1);
+        imageArray.add(image2);
+        imageArray.add(image3);
+        imageArray.add(image4);
+        imageArray.add(image5);
+
+        pagination.setPageFactory((pageIndex) -> {
+            imageNum = pageIndex;
+
+            //splitmenu
+            button1.setOnAction((event)-> {
+                pagination.setCurrentPageIndex(0);
+            });
+            button2.setOnAction((event)-> {
+                pagination.setCurrentPageIndex(1);
+            });
+            button3.setOnAction((event)-> {
+                pagination.setCurrentPageIndex(2);
+            });
+            button4.setOnAction((event)-> {
+                pagination.setCurrentPageIndex(3);
+            });
+            button5.setOnAction((event)-> {
+                pagination.setCurrentPageIndex(4);
+            });
+            imageView.setImage(imageArray.get(imageNum));
+         //   System.out.println("Hi Twyla " + pageIndex);
+            return imageView;
+        });
+
+
+
+
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
 
